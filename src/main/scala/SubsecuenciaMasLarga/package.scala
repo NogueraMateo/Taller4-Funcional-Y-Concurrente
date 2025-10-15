60490bd0fbba3334365c3008e47afba5774de836
NogueraMateo <mateonoguerapinto@gmail.com>
2025-10-15 18:40:17 -0500

    Implemented incremental subsequence problem using brute force

--- src/main/scala/SubsecuenciaMasLarga/package.scala
+++ src/main/scala/SubsecuenciaMasLarga/package.scala
@@ -13,4 +13,50 @@ package object SubsecuenciaMasLarga {
         remaining ++ concatI
       }
     }
+
+  /**
+   * Devuelve la secuencia correspondiente a la secuencia creciente de índices inds
+   * @param s Secuencia de enteros
+   * @param inds Secuencia creciente de índices asociada a s
+   * @return
+   */
+    def subSecuenciaAsoc(s: Secuencia, inds: Seq[Int]): Subsecuencia = {
+      for (ind <- inds) yield s(ind)
+    }
+
+  /**
+   * Devuelve el conjunto de todas las subsecuencias de s
+   * @param s Secuencia de enteros
+   */
+    def subSecuenciasDe(s: Secuencia): Set[Subsecuencia] = {
+      val indexSubsequences = subindices(0, s.length)
+      for (subseq <- indexSubsequences) yield subSecuenciaAsoc(s, subseq)
+    }
+
+  /**
+   * Decide si una secuencia es incremental o no
+   * @param s Secuencia de enteros
+   */
+    def incremental(s: Subsecuencia): Boolean = {
+      val wrongOrder = for {
+        i <- 0 until (s.length - 1) // Índices de s menos el último
+        if s(i) > s(i+1) // Revisa si s(i) es mayor que s(i+1) el siguiente en la secuencia
+      } yield i // Lo recolecta
+
+      // Si el resultado de lo anterior tiene al menos un elemento, entonces, existe al menos un s(i) talque s(i+1) es menor que él
+      // luego la lista no es incremental
+      wrongOrder.isEmpty
+    }
+
+    /**
+     * Devuelve el conjunto de subsecuencias incrementales de s
+     * @param s Secuencia de enteros
+     * @return
+     */
+    def subSecuenciasInc(s: Secuencia): Set[Subsecuencia] = {
+        for {
+          subseq <- subSecuenciasDe(s)
+          if incremental(subseq)
+        } yield subseq
+      }
 }
--- src/test/scala/pruebas.sc
+++ src/test/scala/pruebas.sc
@@ -1,3 +1,13 @@
 import SubsecuenciaMasLarga._
 
 subindices(2, 3)
+
+val s1 = Seq(20, 30, 10)
+
+subSecuenciasDe(s1)
+
+subSecuenciasInc(s1)
+
+val s = List(20, 30, 10, 40, 15, 16, 17)
+
+subSecuenciasInc(s)
