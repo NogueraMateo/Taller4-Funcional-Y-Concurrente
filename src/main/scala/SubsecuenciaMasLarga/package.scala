586be0b0dcfeebe5a5e64ff3a8d1645b1bbbed3e
NogueraMateo <mateonoguerapinto@gmail.com>
2025-10-15 19:15:41 -0500

    Fix syntax errors

--- src/main/scala/SubsecuenciaMasLarga/package.scala
+++ src/main/scala/SubsecuenciaMasLarga/package.scala
@@ -20,48 +20,52 @@ package object SubsecuenciaMasLarga {
   def ssimlComenzandoEn(i: Int, s: Secuencia): Subsecuencia = {
     if (i >= s.length) Seq.empty[Int]
     else {
-      
+
       val subsecuenciasPosteriores = for {
         j <- i + 1 until s.length
         if s(j) > s(i)
       } yield ssimlComenzandoEn(j, s)
-      
-      
+
+
       if (subsecuenciasPosteriores.isEmpty) Seq(s(i))
       else {
-        
+
         val masLarga = subsecuenciasPosteriores.maxBy(_.length)
         s(i) +: masLarga
       }
     }
+  }
 
-  /**
-   * Devuelve la secuencia correspondiente a la secuencia creciente de índices inds
-   * @param s Secuencia de enteros
-   * @param inds Secuencia creciente de índices asociada a s
-   * @return
-   */
+    /**
+     * Devuelve la secuencia correspondiente a la secuencia creciente de índices inds
+     *
+     * @param s    Secuencia de enteros
+     * @param inds Secuencia creciente de índices asociada a s
+     * @return
+     */
     def subSecuenciaAsoc(s: Secuencia, inds: Seq[Int]): Subsecuencia = {
       for (ind <- inds) yield s(ind)
     }
 
-  /**
-   * Devuelve el conjunto de todas las subsecuencias de s
-   * @param s Secuencia de enteros
-   */
+    /**
+     * Devuelve el conjunto de todas las subsecuencias de s
+     *
+     * @param s Secuencia de enteros
+     */
     def subSecuenciasDe(s: Secuencia): Set[Subsecuencia] = {
       val indexSubsequences = subindices(0, s.length)
       for (subseq <- indexSubsequences) yield subSecuenciaAsoc(s, subseq)
     }
 
-  /**
-   * Decide si una secuencia es incremental o no
-   * @param s Secuencia de enteros
-   */
+    /**
+     * Decide si una secuencia es incremental o no
+     *
+     * @param s Secuencia de enteros
+     */
     def incremental(s: Subsecuencia): Boolean = {
       val wrongOrder = for {
         i <- 0 until (s.length - 1) // Índices de s menos el último
-        if s(i) > s(i+1) // Revisa si s(i) es mayor que s(i+1) el siguiente en la secuencia
+        if s(i) > s(i + 1) // Revisa si s(i) es mayor que s(i+1) el siguiente en la secuencia
       } yield i // Lo recolecta
 
       // Si el resultado de lo anterior tiene al menos un elemento, entonces, existe al menos un s(i) talque s(i+1) es menor que él
@@ -71,28 +75,26 @@ package object SubsecuenciaMasLarga {
 
     /**
      * Devuelve el conjunto de subsecuencias incrementales de s
+     *
      * @param s Secuencia de enteros
      * @return
      */
     def subSecuenciasInc(s: Secuencia): Set[Subsecuencia] = {
-        for {
-          subseq <- subSecuenciasDe(s)
-          if incremental(subseq)
-        } yield subseq
+      for {
+        subseq <- subSecuenciasDe(s)
+        if incremental(subseq)
+      } yield subseq
+    }
+
+
+    def subSecIncMasLargaV2(s: Secuencia): Subsecuencia = {
+      if (s.isEmpty) Seq.empty[Int]
+      else {
+        val todasLasSubsecuencias = for {
+          i <- s.indices
+        } yield ssimlComenzandoEn(i, s)
+
+        todasLasSubsecuencias.maxBy(_.length)
       }
-  }
-
-
-
-  def subSecIncMasLargaV2(s: Secuencia): Subsecuencia = {
-    if (s.isEmpty) Seq.empty[Int]
-    else {
-      val todasLasSubsecuencias = for {
-        i <- s.indices
-      } yield ssimlComenzandoEn(i, s)
-
-      todasLasSubsecuencias.maxBy(_.length)
     }
   }
-
-}
